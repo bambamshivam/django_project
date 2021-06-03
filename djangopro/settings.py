5acae66aacec7841915eeff360753cc9d8495abf
Bambam <bambamshivam@gmail.com>
2021-06-03 19:43:32 +0530

    First Commit

--- djangopro/settings.py
+++ djangopro/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'djangopro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog_data',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'Aa1@bcdef',
     }
 }
 
